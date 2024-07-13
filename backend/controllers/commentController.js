2a3ee1d00cddb195e871f4f3204d7ec0c244cb0d
rosecodez <rosebeats09@gmail.com>
2024-07-13 14:48:16 -0400

    Move comment controller from previous repo

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -2,3 +2,101 @@ const User = require("../models/user");
 const Comment = require("../models/comment");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+const User = require("../models/user");
+const Comment = require("../models/comment");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+// Get all comments for a specific post
+const getAllComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find();
+    res.json(comments);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get a specific comment by id for a post
+const getCommentById = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+    res.json(comment);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Create a new comment for a post
+const createComment = async (req, res, next) => {
+  const { text } = req.body;
+  try {
+    const existingComment = await Comment.findOne({ text });
+    if (existingComment) {
+      return res.status(400).json({ message: "Comment already exists" });
+    }
+
+    const user = await User.findById("6679b6dd5b9e70e350d8a210");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const newComment = new Comment({
+      text: "That's really interesting. nice job",
+      timestamp: new Date(),
+      user: user._id,
+      postId: req.params.postId,
+    });
+
+    await newComment.save();
+    res.status(201).json(newComment);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Update a comment by id for a post
+const updateComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    comment.text = "hmm";
+    comment.timestamp = new Date();
+    comment.user = comment.user;
+
+    await comment.save();
+
+    res.json(comment);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Delete a comment by id for a post
+const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    await comment.deleteOne();
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getAllComments,
+  getCommentById,
+  createComment,
+  updateComment,
+  deleteComment,
+};
