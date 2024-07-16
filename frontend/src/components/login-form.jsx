3fc632666dafac97aa7f9154ea869315907bba2b
rosecodez <rosebeats09@gmail.com>
2024-07-15 20:14:52 -0400

    Display error message for signup form

--- frontend/src/components/login-form.jsx
+++ frontend/src/components/login-form.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginForm( { setToken } ) {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const [signupError, setSignupError] = useState('');
 
     const loginUser = async (credentials) => {
         return fetch('http://localhost:8080/login', {
@@ -34,7 +36,7 @@ export default function LoginForm( { setToken } ) {
 
             <div id="signup-container" className='pt-5'>
                 <h6 className='pb-3'>Don't have an account?</h6>
-                <a href='signup'>Log in</a>
+                <a href='signup'>Sign up</a>
             </div>
 
             <b><p className="title pb-2 pt-5">Login Form</p></b>
@@ -45,11 +47,11 @@ export default function LoginForm( { setToken } ) {
                 <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type="username" {...register("username", { required: true })} />
                 {errors.username && <span style = { { color: "red" } }>Username is mandatory</span>}
 
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password" placeholder="password">Password</label>
+                <label className="pt-4 block text-gray-700 text-sm font-bold mb-2" htmlFor="password" placeholder="password">Password</label>
                 <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type="password" {...register("password", { required: true })} />
                 {errors.password && <span style={{ color: "red" }}>Password is mandatory</span>}
 
-                <button className='mt-8 bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type="submit">Log in</button>            
+                <button className='mt-6 bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type="submit">Log in</button>            
             </form>
         </div>
     );
--- frontend/src/components/signup-form.jsx
+++ frontend/src/components/signup-form.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 
 export default function SignupForm() {
     const navigate = useNavigate();
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const [signupError, setSignupError] = useState('');
 
     const signupUser = async (credentials) => {
         try {
@@ -15,22 +16,22 @@ export default function SignupForm() {
                 },
                 body: JSON.stringify(credentials),
             });
-    
+
             if (!response.ok) {
                 const errorData = await response.json();
-                console.log("Signup failed:", errorData.message);
+                setSignupError(`Signup failed: ${errorData.message}`);
                 return;
             }
-    
             const data = await response.json();
             console.log("Signup successful:", data);
-            navigate("/");
+            navigate("/login");
         } catch (error) {
             console.error("Error signing up:", error.message);
         }
     };
 
     const onSubmit = (data) => {
+        setSignupError('');
         signupUser(data);
     };
 
@@ -46,13 +47,15 @@ export default function SignupForm() {
             <form id="signup-form" className='flex flex-col bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4' onSubmit={handleSubmit(onSubmit)}>
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">Username</label>
                 <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type="text" name="username" {...register("username", { required: true })} placeholder="Username" />
-                {errors.username && <span style={{ color: "red" }}>Username is mandatory. Minimum 6 letters</span>}
+                {errors.username && <span className='pt-2' style={{ color: "red" }}>Username is mandatory. Minimum 6 letters</span>}
                 
                 <label className="pt-4 block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
                 <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type="password" name="password" {...register("password", { required: true })} placeholder="Password" />
-                {errors.password && <span style={{ color: "red" }}>Password is mandatory. Minimum 10 letters</span>}
+                {errors.password && <span className='pt-2' style={{ color: "red" }}>Password is mandatory. Minimum 10 letters</span>}
 
-                <button className='mt-8 bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type="submit">Sign Up</button>
+                {signupError && <span className='pt-4' style={{ color: "red" }}>{signupError}</span>}
+                
+                <button className='mt-6 bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type="submit">Sign Up</button>
             </form>
         </div>
     );
